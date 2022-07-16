4646a778b6795a3cb1d0b50514e9e45506895fb9
Tianjie(TJ) Zhang <tjzhang@u.boisestate.edu>
2022-07-15 18:00:26 -0600

    1

--- gan_for_crack/modified_DCGAN.py
+++ gan_for_crack/modified_DCGAN.py
@@ -89,17 +89,42 @@ class Generator(nn.Module):
         self.l1 = nn.Sequential(nn.Linear(opt.latent_dim, 128 * self.init_size ** 2))
 
         self.conv_blocks = nn.Sequential(
-            nn.BatchNorm2d(128),
-            nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 128, 3, stride=1, padding=1),
-            nn.BatchNorm2d(128, 0.8),
+
+            nn.ConvTranspose2d(),
+            nn.LeakyReLU(0.2, inplace=True),
+
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
             nn.LeakyReLU(0.2, inplace=True),
             nn.Upsample(scale_factor=2),
-            nn.Conv2d(128, 64, 3, stride=1, padding=1),
-            nn.BatchNorm2d(64, 0.8),
-            nn.LeakyReLU(0.2, inplace=True),
-            nn.Conv2d(64, opt.channels, 3, stride=1, padding=1),
-            nn.Tanh(),
+
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
+            nn.LeakyReLU(),
+            nn.Upsample(),
+
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
+            nn.LeakyReLU(),
+            nn.Upsample(),
+
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
+            nn.LeakyReLU(),
+            nn.Upsample(),
+
+            nn.Conv2d(),
+            nn.BatchNorm2d(),
+            nn.LeakyReLU(),
+            nn.Upsample(),
+
+            nn.Conv2d(),
+            nn.LeakyReLU(),
+            nn.Upsample(),
+
+            nn.Conv2d(),
+            nn.Sigmoid(),
+
         )
 
     def forward(self, z):
